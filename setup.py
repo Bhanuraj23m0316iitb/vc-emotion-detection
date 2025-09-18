57b97456c2bfd47ee7184e86a8809c6e32ff59c3
Bhanuraj Badal <badalbhanuraj@gmail.com>
2025-09-18 11:35:43 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this project is use to get emotion from raw text using dvc',
+    author='Bhanuraj Badal',
+    license='',
+)
